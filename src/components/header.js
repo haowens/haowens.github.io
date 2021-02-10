f45abd801ae1798dc41c953d40d4e80db75c8ff0
Hayley Owens <haowens@email.unc.edu>
2021-02-09 23:13:35 -0500

    website take 6 i think

--- src/components/header.js
+++ src/components/header.js
@@ -8,7 +8,7 @@ const Header = () => (
           <h1 className="site-title">
           Hayley's ~Website~   ...
           <a href="https://www.instagram.com/hhayleyowens/" target="_blank"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/1200px-Instagram_logo_2016.svg.png" height="60"></img></a>
-          <a href="https://twitter.com/hhayleyowens"><img src="https://i.pinimg.com/originals/28/90/ba/2890bac9ba41e52707f36268231dfe9e.png" height="61"></img></a>
+          <a href="https://twitter.com/hhayleyowens" target="_blank"><img src="https://i.pinimg.com/originals/28/90/ba/2890bac9ba41e52707f36268231dfe9e.png" height="61"></img></a>
           </h1>
       </div>
     </div>
