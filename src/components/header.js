09690f48c05f10ffb9191ae94eb1cf59ee7e7f42
Hayley Owens <haowens@email.unc.edu>
2021-02-09 20:57:15 -0500

    essentially website at this point

--- src/components/header.js
+++ src/components/header.js
@@ -5,9 +5,11 @@ const Header = () => (
   <header className="masthead">
     <div>
       <div>
-        <Link to="/">
-          <h1 className="site-title">Hayley's Website</h1>
-        </Link>
+          <h1 className="site-title">
+          Hayley's ~Website~   ...
+          <a href="https://www.instagram.com/hhayleyowens/" target="_blank"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/1200px-Instagram_logo_2016.svg.png" height="60"></img></a>
+          <a href="https://twitter.com/hhayleyowens"><img src="https://i.pinimg.com/originals/28/90/ba/2890bac9ba41e52707f36268231dfe9e.png" height="61"></img></a>
+          </h1>
       </div>
     </div>
   </header>
